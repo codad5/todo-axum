d0c5c1eff781db35aa350b1bf70f71082dabf700
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2024-05-26 01:39:48 +0100

    Update server address to listen on all interfaces and port 80

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,21 @@ async fn main( ) {
     println!("Starting server app : {}", std::env::var("CARGO_PKG_NAME").unwrap());
     let app = Router::new()
         .route("/", get(hello));
+    
+    const HOSTS : [[u8; 4]; 2] = [[127, 0, 0, 1], [0, 0, 0, 0]];
+    let port : u16 = get_port();
+    // try connecting to any host , the host that first connect without error will be used
+    while let Some(host) = HOSTS.iter().next() {
+        let addr = SocketAddr::from((*host, port));
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 80));
-    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
-    println!("Listening on {}", addr);
-    axum::serve(listener, app).await.unwrap();
+        let listener = tokio::net::TcpListener::bind(&addr).await;
+        if listener.is_ok() {
+            println!("Listening on {}", addr);
+            axum::serve(listener.unwrap(), app).await.unwrap();
+            break;
+        }
+    }
+
 }
 
 
@@ -27,4 +37,11 @@ async fn hello() -> Json<Message> {
     Json(Message {
         message: "Hello, World!".to_string(),
     })
+}
+
+fn get_port() -> u16 {
+    std::env::var("PORT")
+        .ok()
+        .and_then(|p| p.parse().ok())
+        .unwrap_or(3000)
 }
